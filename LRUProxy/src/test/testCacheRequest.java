604052a7d6a448ec60445469e52f3dca27bfedc9
bryantj10 <bryantj10@nku.edu>
2015-10-01 11:43:20 -0400

    Wrote testCacheRequest.java

--- LRUProxy/src/test/testCacheRequest.java
+++ LRUProxy/src/test/testCacheRequest.java
@@ -1,4 +1,18 @@
 package test;
-public class testCacheRequest{
-	
+import static org.junit.Assert.*;
+
+import java.CacheRequest;
+
+import org.junit.*;
+
+public class testCacheRequest {
+	String directory = "C:\\Users\\catfa_000\\workspace\\LRUProxy(1)\\LRUProxy\\src\\java\\";
+	CacheRequest request = new CacheRequest(directory);
+
+	@Test
+	public void testRead(){
+	    String expected = "www.google.com";
+	    assertEquals(expected, request.read());
+	    
+	}
 }
